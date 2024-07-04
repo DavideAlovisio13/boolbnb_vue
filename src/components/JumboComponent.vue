765dbd8096505190b75146e7ebaab6136a15ccb9
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-04 23:18:54 +0200

    add images and video

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="masthead text-center text-white">
         <div class="masthead-content">
-            <div class="container px-5">
-                <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-                <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+            <div class="container px-5 d-flex justify-content-center align-items-center">
                 <SearchComponent />
             </div>
         </div>
@@ -27,8 +25,8 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: red;
-
+    position: relative;
+    
     .container {
         display: flex;
         flex-direction: column;
