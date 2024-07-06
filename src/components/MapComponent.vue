1904d0ef3ed735567317d5e3a820e0748573ce01
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-06 12:07:54 +0200

    update map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -8,17 +8,35 @@ import tt from '@tomtom-international/web-sdk-maps';
 export default {
     name: 'MapComponent',
     props: {
-        apartment: Object,
+        apartment: {
+            type: Object,
+            required: true,
+            default: () => ({ latitude: '0', longitude: '0' }) // Coordinate di default
+        },
     },
     mounted() {
         this.initializeMap();
     },
     methods: {
         initializeMap() {
+            console.log(this.apartment);
+            // Log per verificare i valori delle coordinate
+            console.log('Latitude:', this.apartment.latitude);
+            console.log('Longitude:', this.apartment.longitude);
+
+            const latitude = parseFloat(this.apartment.latitude);
+            const longitude = parseFloat(this.apartment.longitude);
+
+            // Controllo se le coordinate sono numeri validi
+            if (isNaN(latitude) || isNaN(longitude)) {
+                console.error('Invalid latitude or longitude');
+                return;
+            }
+
             const map = tt.map({
                 key: '88KjpqU7nmmEz3D6UYOg0ycCp6VqtdXI', // Sostituisci con la tua API Key
                 container: 'map',
-                center: [this.apartment.longitude, this.apartment.latitude], // Coordinate per il centro della mappa
+                center: [longitude, latitude], // Coordinate per il centro della mappa
                 zoom: 10
             });
         }
