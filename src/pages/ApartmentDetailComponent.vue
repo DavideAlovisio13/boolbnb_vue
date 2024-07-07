7a7063b2df14041c564d134ae390e84d3fba9d4d
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-07 18:43:58 +0200

    fix store.js apiBaseUrl, added form to send message in ApartmentDetailComponent

--- src/pages/ApartmentDetailComponent.vue
+++ src/pages/ApartmentDetailComponent.vue
@@ -9,7 +9,32 @@
             <p>Bagni: {{ apartment.num_bathrooms }}</p>
             <p>Metri quadrati: {{ apartment.square_meters }}</p>
             <p>Indirizzo: {{ apartment.address }}</p>
-            <MapComponent :apartment="apartment"/>
+            <div class="row">
+                <div class="col-6">
+                    <MapComponent :apartment="apartment" />
+                </div>
+                <div class="col-6">
+                    <form @submit.prevent="sendMessage" method="POST">
+                        <div>
+                            <label for="name">Nome:</label>
+                            <input type="text" id="name" v-model="message.name" required>
+                        </div>
+                        <div>
+                            <label for="surname">Cognome:</label>
+                            <input type="text" id="name" v-model="message.surname" required>
+                        </div>
+                        <div>
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" v-model="message.email" required>
+                        </div>
+                        <div>
+                            <label for="content">Messaggio:</label>
+                            <textarea id="content" v-model="message.body" required></textarea>
+                        </div>
+                        <button type="submit">Invia Messaggio</button>
+                    </form>
+                </div>
+            </div>
         </div>
         <p v-else>Loading...</p>
     </div>
@@ -17,6 +42,8 @@
 
 <script>
 import MapComponent from '@/components/MapComponent.vue';
+import axios from 'axios';
+import { store } from '@/store';
 
 export default {
     name: 'ApartmentDetailComponent',
@@ -26,7 +53,14 @@ export default {
     props: ['id'],
     data() {
         return {
-            apartment: {}
+            store,
+            apartment: {},
+            message: {
+                name: '',
+                surname: '',
+                email: '',
+                body: ''
+            }
         };
     },
     computed: {
@@ -34,7 +68,7 @@ export default {
             return this.apartment.cover_image ? `http://127.0.0.1:8000/storage/${this.apartment.cover_image}` : 'https://via.placeholder.com/320x240';
         }
     },
-    created() {
+    mounted() {
         this.fetchApartmentDetails();
     },
     methods: {
@@ -43,6 +77,20 @@ export default {
                 .then(response => response.json())
                 .then(data => {
                     this.apartment = data.results;
+                });
+        },
+        sendMessage() {
+            // Invia il messaggio al back end (Laravel)
+            console.log(this.message);
+            axios.post(`${this.store.apiBaseUrl}/apartments/${this.id}/send-message`, this.message)
+                .then(response => {
+                    console.log(response);
+                    alert('Messaggio inviato con successo!');
+                    this.message = { name: '',surname: '', email: '', body: '' }; // Pulisce il form dopo l'invio
+                })
+                .catch(error => {
+                    console.error('Errore durante l\'invio del messaggio:', error);
+                    
                 });
         }
     }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 export const store = reactive({
-  apiBaseUrl: "http://127.0.0.1:8000/Api",
+  apiBaseUrl: "http://127.0.0.1:8000/api",
   imgBasePath: "http://127.0.0.1:8000/storage/",
   apartments: [],
   search: [],
