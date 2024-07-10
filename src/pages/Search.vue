5cae865c524a546c798389b5c917f1a2d9acf46d
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-10 13:06:12 +0200

    da controllare caso limite

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,23 +1,55 @@
 <template>
     <div class="container text-center mt-5">
         <SearchComponent @search-performed="getApartments" />
+        <h4>Servizi</h4>
         <div class="d-flex justify-content-between align-items-center">
-            <div v-for="(service, index) in services" :key="index" 
-            :class="{ 'selected-service': selectedServiceId.includes(service.id) }"
-            class="service-item w-25" @click="toggleServiceSelection(service.id)">
+            <div v-for="(service, index) in services" :key="index"
+                :class="{ 'selected-service': selectedServiceId.includes(service.id) }" class="service-item w-25"
+                @click="toggleServiceSelection(service.id)">
                 <img :src="getServiceIconUrl(service.icon)" :alt="service.name" class="w-25" />
                 <p>{{ service.name }}</p>
             </div>
         </div>
-            <p>Numero di risultati: {{ this.allApartments.length }}</p>
-            <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                <div class="col m-5" v-for="(item, index) in allApartments" :key="index">
-                    <CardApComponent :apartment="item" :index="index" :title="item.name" :image="item.cover_image"
-                        :num_rooms="item.num_rooms" :num_beds="item.num_beds" :class="['bg-warning', 'sponsored']"
-                        :distance="item.distance" />
-                </div>
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex flex-column">
+                <label for="num_rooms">Numero camere</label>
+                <select name="num_rooms" id="num_rooms" v-model="num_rooms">
+                    <option value="all">Qualsiasi</option>
+                    <option value="1">1 camera</option>
+                    <option value="2">2 camere</option>
+                    <option value="3">3 camere</option>
+                    <option value="4">4 camere</option>
+                    <option value="5">5 o più camere</option>
+                </select>
+            </div>
+            <div class="d-flex flex-column">
+                <label for="num_rooms">Numero letti</label>
+                <select name="num_rooms" id="num_rooms" v-model="num_beds">
+                    <option value="all">Qualsiasi</option>
+                    <option value="1">1 posto letto</option>
+                    <option value="2">2 posti letto</option>
+                    <option value="3">3 posti letto</option>
+                    <option value="4">4 posti letto</option>
+                    <option value="5">5 o più posti letto</option>
+                </select>
+            </div>
+            <div class="d-flex flex-column">
+                <label for="distance">Distanza</label>
+                <input type="range" v-model="distance" name="distance" id="distance" min="3" max="50" value="20"
+                    step="1">
+                <span>{{ distance }} km</span>
+            </div>
+            <button class="btn btn-primary" @click="getAdvancedSearchResults">Applica filtri</button>
+        </div>
+        <p>Numero di risultati: {{ this.allApartments.length }}</p>
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            <div class="col m-5" v-for="(item, index) in allApartments" :key="index">
+                <CardApComponent :apartment="item" :index="index" :title="item.name" :image="item.cover_image"
+                    :num_rooms="item.num_rooms" :num_beds="item.num_beds" :class="['bg-warning', 'sponsored']"
+                    :distance="item.distance" />
             </div>
         </div>
+    </div>
 </template>
 
 <script>
@@ -44,7 +76,10 @@ export default {
             filteredApartmentsBase: [],
             filteredApartmentsSponsored: [],
             allApartments: [],
-            selectedServiceId: []
+            selectedServiceId: [],
+            num_beds: 'all',
+            num_rooms: 'all',
+            distance: 20
         }
     },
 
@@ -70,21 +105,39 @@ export default {
             } else {
                 this.selectedServiceId.splice(index, 1);
             }
-            this.getFilterServices();
+            this.getAdvancedSearchResults();
         },
 
+        // getFilterServices() {
+        //     axios.get(`${this.store.apiBaseUrl}apartments/services-filter/${this.searchAddress}/${this.latitude}/${this.longitude}/${this.selectedServiceId}`)
+        //         .then((response) => {
+        //             this.filteredApartmentsBase = response.data.results.base;
+        //             this.filteredApartmentsSponsored = response.data.results.sponsored;
+        //             this.allApartments = [...this.filteredApartmentsSponsored, ...this.filteredApartmentsBase];
+        //         }).catch((error) => {
+        //             console.error('API error:', error);
+        //         })
+        // },
 
-        getFilterServices() {
-        axios.get(`${this.store.apiBaseUrl}apartments/services-filter/${this.searchAddress}/${this.latitude}/${this.longitude}/${this.selectedServiceId}`)
-        .then((response) => { 
-            this.filteredApartmentsBase = response.data.results.base;
-            this.filteredApartmentsSponsored = response.data.results.sponsored;
-            this.allApartments = [...this.filteredApartmentsSponsored, ...this.filteredApartmentsBase];
-        }).catch((error) => {
-            console.error('API error:', error);
-        })
-    },
-    
+        getAdvancedSearchResults() {
+            const query = this.searchAddress;
+            const latitude = this.latitude;
+            const longitude = this.longitude;
+            const rooms = this.num_rooms ;
+            const beds = this.num_beds ;
+            const range = this.distance ;
+            const serviceIds = this.selectedServiceId.length > 0 ? this.selectedServiceId.join(',') : 'all';
+            const url = `${this.store.apiBaseUrl}apartments/advanced-search/${encodeURIComponent(query)}/${latitude}/${longitude}/${serviceIds}/${rooms}/${beds}/${range}`;
+            axios.get(url)
+                .then((response) => {
+                    this.filteredApartmentsBase = response.data.results.base;
+                    this.filteredApartmentsSponsored = response.data.results.sponsored;
+                    this.allApartments = [...this.filteredApartmentsSponsored, ...this.filteredApartmentsBase];
+                }).catch((error) => {
+                    console.error('API error:', error);
+                })
+        },
+
         extractParamsFromUrl(url) {
             const parts = url.split('/');
             const query = decodeURIComponent(parts[parts.indexOf('search') + 1]);
