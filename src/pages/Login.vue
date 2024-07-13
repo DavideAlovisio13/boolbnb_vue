19361ce37782aa938dce5d8311456734a115596b
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-13 18:35:22 +0200

    modify style of bootns in login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,54 +1,61 @@
 <template>
-    <div class="my-container"
+  <div class="my-container"
     style="background-image: url(/images/bg-main.png); background-repeat: no-repeat; background-size: cover">
-        <div class="container">
-            <div class="apartment-details brutalist-card mt-2">  
-                <h1 class="pb-3 text-center">Benvenuto!</h1>
-                <div class="d-flex justify-content-center">
-                    <a href="http://127.0.0.1:8000/login" class="btn btn-success">Login</a>
-                </div>
-
-                <p class="pt-3 text-center">Registrati e inizia a guadagnare con noi</p>
-                <div class="d-flex justify-content-center">
-                    <a href="http://127.0.0.1:8000/register" class="btn btn-primary">Registrati</a>    
-                </div>
-            </div>
+    <div class="container">
+      <div class="apartment-details brutalist-card mt-2">
+        <h2 class=" text-center pt-3">Accedi e inserisci il tuo appartamento</h2>
+        <div class="d-flex justify-content-center">
+          <a href="http://127.0.0.1:8000/login" class="btn nav-item input__button__shadow">Login</a>
         </div>
+
+        <h2 class=" text-center pt-3">Registrati e inizia a guadagnare con noi</h2>
+        <div class="d-flex justify-content-center">
+          <a href="http://127.0.0.1:8000/register" class="btn nav-item input__button__shadow">Registrati</a>
+        </div>
+      </div>
     </div>
+  </div>
 
 </template>
 
 <script>
-    export default {
-        name:'Login',
-    }
+export default {
+  name: 'Login',
+}
 </script>
 
 <style lang="scss" scoped>
-.my-container{
+.my-container {
   height: 100vh;
 }
-        .container{
-        padding-top: 200px;
-    }
-    a {
-      color: white;
-      text-decoration: none;
-    }
-    a:hover {
-      font-weight: bold;
-    }
-    h1{
-      color: black;
-    }
-    a {
-      color: white;
-      text-decoration: none;
-    }
-    a:hover {
-      font-weight: bold;
-    }
-    .brutalist-card {
+
+.container {
+  padding-top: 200px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  font-weight: bold;
+}
+
+h1 {
+  color: black;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  font-weight: bold;
+}
+
+.brutalist-card {
   width: 75%;
   margin: 0 auto;
   border: 4px solid #000;
@@ -130,4 +137,34 @@
   box-shadow: none;
 }
 
+/* Button Styles for Navigation Items */
+.input__button__shadow {
+  cursor: pointer;
+  border: 3px solid #000;
+  background: #e9b50b;
+  transition: all 400ms cubic-bezier(0.23, 1, 0.32, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  transform: translateZ(20px);
+  position: relative;
+  z-index: 3;
+  font-weight: bold;
+  font-family: "Courier New", monospace;
+}
+
+/* Hover effect for Navigation Buttons */
+.input__button__shadow:hover {
+  background: #e9b50b;
+  transform: translateZ(10px) translateX(-5px) translateY(-5px);
+  box-shadow: 5px 5px 0 0 #000;
+}
+
+/* SVG Icon Styles within Buttons */
+.input__button__shadow svg {
+  fill: #000;
+  width: 25px;
+  height: 25px;
+}
 </style>
