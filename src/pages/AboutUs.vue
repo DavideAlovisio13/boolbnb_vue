27e66866077513b637c44ee049dfd35284f3a9ed
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-13 18:37:47 +0200

    refactory of aboutus

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,70 +1,77 @@
 <template>
-    <div class="my-container"
+  <div class="my-container"
     style="background-image: url(/images/bg-main.png); background-repeat: no-repeat; background-size: cover">
     <div class="container">
-    <div class="apartment-details brutalist-card mt-2"> 
+      <div class="apartment-details brutalist-card mt-2">
 
-    <section class="hero">    
-        <h1 class="d-flex align-items-center justify-content-center"> <i class="fa-solid fa-earth-americas pe-2"></i>Scopri Boolbnb</h1>
-        <div class="d-flex align-items-center justify-content-center pb-3">
+        <section class="hero">
+          <h1 class="d-flex align-items-center justify-content-center"> <i
+              class="fa-solid fa-earth-americas pe-2"></i>Scopri Boolbnb</h1>
+          <div class="d-flex align-items-center justify-content-center pb-3">
             <span>Connettiamo viaggiatori con alloggi unici in tutto il mondo.</span>
-        </div>
-    </section>
+          </div>
+        </section>
 
-    <section class="mission">
-           <h4> <i class="fa-solid fa-plane pe-2"></i>
-             La nostra missione</h4>
-        <div class="d-flex align-items-center justify-content-center pb-3">
+        <section class="mission">
+          <h4> <i class="fa-solid fa-plane pe-2"></i>
+            La nostra missione</h4>
+          <div class="d-flex align-items-center justify-content-center pb-3">
             <span> Offrire esperienze di viaggio autentiche e memorabili, sostenendo le economie locali.</span>
-        </div>
-    </section>
-    
-    <section class="values">
-        <h2 class="pb-3">I nostri valori</h2> 
-        <h4> <i class="fa-solid fa-shield-halved pe-2"></i>Affidabilità</h4>
-        <div class="d-flex align-items-center justify-content-center pb-3">  
-            <span>Garantiamo sicurezza e fiducia per ogni prenotazione.</span >
-        </div>
+          </div>
+        </section>
 
-        <h4><i class="fa-regular fa-handshake pe-2"></i>Ospitalità</h4>
-        <div class="d-flex align-items-center justify-content-center pb-3">
+        <section class="values">
+          <h2 class="pb-3">I nostri valori</h2>
+          <h4> <i class="fa-solid fa-shield-halved pe-2"></i>Affidabilità</h4>
+          <div class="d-flex align-items-center justify-content-center pb-3">
+            <span>Garantiamo sicurezza e fiducia per ogni prenotazione.</span>
+          </div>
+
+          <h4><i class="fa-regular fa-handshake pe-2"></i>Ospitalità</h4>
+          <div class="d-flex align-items-center justify-content-center pb-3">
             <span>Accogliamo i nostri ospiti con calore e professionalità.</span>
-        </div>
+          </div>
 
-        <h4><i class="fa-solid fa-seedling pe-2"></i>Sostenibilità</h4>
-        <div class="d-flex align-items-center justify-content-center pb-3">
+          <h4><i class="fa-solid fa-seedling pe-2"></i>Sostenibilità</h4>
+          <div class="d-flex align-items-center justify-content-center pb-3">
             <span>Promuoviamo pratiche rispettose dell'ambiente.</span>
-        </div>
-    </section>
-</div>
-</div>
+          </div>
+        </section>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:'AboutUs',
-    }
+export default {
+  name: 'AboutUs',
+}
 </script>
 
 <style lang="scss" scoped>
-    .my-container{
-    height: 100%;
-    }
-    .container{
-        padding-top: 200px;
-        padding-bottom: 100px;
-    }
-    h2, h4, span {
-      color: black;
-      text-align: center;
-    }
-    h1{
-        font-weight: bold;
-        color: #E9B50D ;
-        text-shadow: 1px 2px 2px rgba(145, 142, 142, 0.7);
-    }
-    .brutalist-card {
+.my-container {
+  height: 100%;
+}
+
+.container {
+  padding-top: 200px;
+  padding-bottom: 100px;
+}
+
+h2,
+h4,
+span {
+  color: black;
+  text-align: center;
+}
+
+h1 {
+  font-weight: bold;
+  color: #E9B50D;
+  text-shadow: 1px 2px 2px rgba(145, 142, 142, 0.7);
+}
+
+.brutalist-card {
   width: 75%;
   margin: 0 auto;
   border: 4px solid #000;
@@ -145,5 +152,4 @@
   transform: translate(5px, 5px);
   box-shadow: none;
 }
-
 </style>
